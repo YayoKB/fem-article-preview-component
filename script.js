e892e03314c597df37754c08d17ffd88c9a3d55d
Kate <38580104+YayoKB@users.noreply.github.com>
2022-07-25 21:47:41 +0200

    update transition to opacity, not class toggle

--- script.js
+++ script.js
@@ -1,24 +1,11 @@
 const shareStart = document.querySelector(".share-icon");
 const shareDiv = document.querySelector(".share");
-const authorDiv = document.querySelector(".author");
 const shareEnd = document.querySelector(".shared");
 
 shareStart.addEventListener("click", (e) => {
-  //if mobile view
-  if (window.innerWidth < 900) {
-    //show ShareDiv and hide authorDiv
-    shareDiv.classList.toggle("display-flex");
-    authorDiv.classList.toggle("display-none");
-  }
-
-  if (window.innerWidth >= 900) {
-    shareDiv.classList.toggle("display-flex");
-  }
+  shareDiv.classList.toggle("show");
 });
 
 shareEnd.addEventListener("click", (e) => {
-  if (window.innerWidth < 900) {
-    shareDiv.classList.toggle("display-flex");
-    authorDiv.classList.toggle("display-none");
-  }
+  shareDiv.classList.toggle("show");
 });
